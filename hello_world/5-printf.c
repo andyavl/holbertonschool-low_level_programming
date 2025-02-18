4c79ddf34f43d39d5fa596d3a377fded7ee0b70c
andyavl <andyavl@hotmail.com>
2025-02-18 09:28:07 -0400

    Task 5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - writes a string or letter.
+ *
+ * Return: value 0.
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
