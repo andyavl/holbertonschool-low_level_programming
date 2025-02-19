69ba11d2c3e94a50c89b8aa1adc16a73bf2343d9
andyavl <andyavl@hotmail.com>
2025-02-19 13:55:14 -0400

    Task 5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet
+ * Return: value 0.
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar ('\n');
+
+	return (0);
+}
