661eebbd24e535e89db19e02f13d79442f3a8088
andyavl <andyavl@hotmail.com>
2025-02-19 12:24:15 -0400

    Task 4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet
+ * Return: value 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+		}
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
