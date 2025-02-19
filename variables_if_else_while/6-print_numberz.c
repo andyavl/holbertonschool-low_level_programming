2e9a58204402ae23100f4cb3843c2adeaa654858
andyavl <andyavl@hotmail.com>
2025-02-19 14:03:01 -0400

    Task 6. Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints numbers
+ * Return: value 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	putchar ('\n');
+
+	return (0);
+}
