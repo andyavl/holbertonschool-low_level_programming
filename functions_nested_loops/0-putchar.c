16d871c071709daf7ca19e1d3a6b2870666557eb
andyavl <andyavl@hotmail.com>
2025-02-20 13:35:24 -0400

    Task 0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - calls the fuction
+ *
+ * Return: value 0.
+ *
+ * _putchar - prints a character
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	return (0);
+}
