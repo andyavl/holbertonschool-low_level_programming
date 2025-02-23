4337dec5a34b171532441454aabcd00004bb6dec
andyavl <andyavl@hotmail.com>
2025-02-23 04:50:36 -0400

    Task 10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sums two int
+ * @a: first input
+ * @b: second input
+ * Return: returns the sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
