808a3234b2267ed3da101213eab53d5f2b6eeca9
andyavl <andyavl@hotmail.com>
2025-02-23 03:07:38 -0400

    Task 8. Im federal agent Jack Bauer, and today is the longest day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 
 	for (hours = 0; hours < 24; hours++)
 	{
-		for(minutes = 0; minutes < 60; minutes++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar('0' + (hours / 10));
 			_putchar('0' + (hours % 10));
