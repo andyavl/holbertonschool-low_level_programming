ba17cdff13cb88e0a9691cd70488fa74a4820a0b
andyavl <andyavl@hotmail.com>
2025-02-27 15:45:02 -0400

    Task 5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,12 +8,14 @@ void rev_string(char *s)
 {
 	int i;
 	int ln;
-	char temp[10];
+	char temp[500];
 
 	for (ln = 0; *s != '\0'; ln++)
 	s++;
 
 	s = s - ln;
+
+
 
 	for (i = 0; i < ln; i++)
 	temp[i] = s[ln - i - 1];
