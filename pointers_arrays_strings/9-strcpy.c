5679e84d0cb7d74762bd3ffb67d0b40b64908c2f
andyavl <andyavl@hotmail.com>
2025-02-28 00:17:21 -0400

    Task 9. strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,7 @@
  * *_strcpy - copy source to a destination
  * @dest: destination
  * @src: source
+ * Return: returns copy
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -12,5 +13,5 @@ char *_strcpy(char *dest, char *src)
 	for (ln = 0; src[ln] != '\0'; ln++)
 	dest[ln] = src[ln];
 
-	return(dest);
+	return (dest);
 }
