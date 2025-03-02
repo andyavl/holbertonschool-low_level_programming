d725d21d8d55a6e56bb18496a3aef6d6409eb0de
andyavl <andyavl@hotmail.com>
2025-03-02 02:31:53 -0400

    Task 2. strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: destination
  * @src: source
  * @n: byte limit from src
- * Return: returns concatenated strings
+ * Return: returns copy
  */
 char *_strncpy(char *dest, char *src, int n)
 {
