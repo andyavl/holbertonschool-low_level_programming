6b00cead890f2dbbe339cc39f90d6c5d29df285d
andyavl <andyavl@hotmail.com>
2025-03-02 15:19:32 -0400

    Task 6. Expect the best. Prepare for the worst. Capitalize on what comes

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@ char *cap_string(char *s)
 {
 	char az, azU;
 	char sep[] = {' ', '\t', '\n', ',', ';', '.',
-					'!', '?', '"', '(', ')', '{', '}', '\0'};
+					'!', '?', '"', '(', ')', '{', '}'};
 	int i, j;
 
 	for (azU = 'A', az = 'a'; az <= 'z'; azU++, az++)
@@ -17,7 +17,7 @@ char *cap_string(char *s)
 		for (i = 0; s[i] != '\0'; i++)
 		{
 			for (j = 0; sep[j] != '\0'; j++)
-			if (s[i - 1] == sep[j] && s[i] == az)
+			if ((s[i - 1] == sep[j] || s[i - 1] == '\0') && s[i] == az)
 			{
 				s[i] = azU;
 			}
