11834579fa01a68477c12c528cb4f71189411a6d
andyavl <andyavl@hotmail.com>
2025-03-05 11:25:27 -0400

    Task 2. strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * *_strchr - locates a char in a string
+ * @s: string
+ * @c: character
+ * Return: returns first occurrence or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		return (s);
+
+		s++;
+	}
+	return (s);
+}
