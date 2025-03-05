99efc157022cc0f9b00323186807195719d58ef1
andyavl <andyavl@hotmail.com>
2025-03-05 09:27:38 -0400

    Task 0. memset

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: pointer to memory
+ * @b: bytes
+ * @n: number of bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n ; i++)
+	s[i] = b;
+
+	dest = src;
+
+	return(dest);
+}
