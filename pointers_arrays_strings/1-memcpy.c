d0a47afd21da9e978ff2d5e333ccaa91e38a885a
andyavl <andyavl@hotmail.com>
2025-03-05 09:51:25 -0400

    Task 1. memcpy

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  * @src: source to copy
  * @dest: destination
  * @n: number of copies
+ * Return: copy
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -12,5 +13,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n ; i++)
 	dest[i] = src[i];
 
-	return(dest);
+	return (dest);
 }
