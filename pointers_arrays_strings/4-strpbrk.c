3fe66d7853959dd14dcef47a6f89801c0b6c793c
andyavl <andyavl@hotmail.com>
2025-03-06 00:46:00 -0400

    Task 4. strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,25 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * *_strbrk - searches a string for any of a set byte
+ * *_strpbrk - searches a string for any of a set byte
  * @s: string
  * @accept: accepted byte
  * Return: returns first occurrence or NULL if not found
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int i;
+
 	while (*s != '\0')
 	{
-		while (*accept != '\0')
+		for (i = 0; accept[i] != '\0'; i++)
 		{
-			if (*s == *accept)
+			if (*s == accept[i])
 			return (s);
-
-			accept++;
 		}
 		s++;
 	}
-	if (*s == *accept)
+	if (*s == accept[i])
 		return (s);
 	else
 		return (0);
