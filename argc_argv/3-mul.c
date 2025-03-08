d37deb339c2e7a105b96f32a18e565a4ca9e288c
andyavl <andyavl@hotmail.com>
2025-03-08 14:06:43 -0400

    Task 3. Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies with its arguments
+ * Return: returns 0;
+ * @argc: amount command line elements
+ * @argv: a pointer to string of command line elements
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int mult = 1;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mult = mult * atoi(argv[i]);
+		}
+		printf("%d\n", mult);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
