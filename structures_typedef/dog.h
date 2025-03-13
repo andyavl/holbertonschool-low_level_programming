b323e9ef4e4a0ff06323e111c63e9f1f4acf9b6c
andyavl <andyavl@hotmail.com>
2025-03-13 15:31:37 -0400

    Task 0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - describes the firulais
+ * @name: firulais name
+ * @age: firulais age
+ * @owner: firulais owner
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
