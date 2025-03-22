3f442821be651aad20cd39a97c749898208cda6c
andyavl <andyavl@hotmail.com>
2025-03-21 20:59:38 -0400

    Task 2. One womans constant is another womans variable

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		text = va_arg(arg, char*);
-		if (text == NULL || *text == '\0')
+		if (text == NULL)
 			text = "(nil)";
 
 		printf("%s", text);
