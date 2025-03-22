f73b24db9316c75e54e94b39756b95868bd783c3
andyavl <andyavl@hotmail.com>
2025-03-22 02:34:28 -0400

    Task 3. To be is a to be the value of a variable

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,81 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * type_char - prints char
+ * @arg: arg to print
+ */
+void type_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+
+/**
+ * type_int - prints int
+ * @arg: arg to print
+ */
+void type_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * type_float - prints float
+ * @arg: arg to print
+ */
+void type_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * type_string - prints string
+ * @arg: arg to print
+ */
+void type_string(va_list arg)
+{
+	char *text;
+
+	text = va_arg(arg, char*);
+	if (text == NULL)
+		text = "(nil)";
+	printf("%s", text);
+}
+
+/**
+ * print_all - prints anything
+ * @format: argument types
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arg;
+	unsigned int i = 0, j = 0;
+	char *coma = "";
+
+	search type[] = {
+		{"c", type_char},
+		{"i", type_int},
+		{"f", type_float},
+		{"s", type_string},
+		{NULL, NULL}
+	};
+	va_start(arg, format);
+	while (format[i])
+	{
+		while (type[j].src != NULL)
+		{
+			if (format[i] == *(type[j].src))
+			{
+				printf("%s", coma);
+				type[j].f(arg);
+				coma = ", ";
+			}
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	printf("\n");
+	va_end(arg);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,21 @@
 #ifndef VAR_FUNCS
 #define VAR_FUNCS
+#include <stdarg.h>
+
+/**
+ *  struct t - struct for correct type
+ *  @src: type
+ *  @f: fuction associated
+ */
+typedef struct t
+{
+char *src;
+void (*f)(va_list arg);
+} search;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
