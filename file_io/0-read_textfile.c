92ed48813a3bec27ad8a32fccd520e5e40cbd4e6
andyavl <andyavl@hotmail.com>
2025-04-05 22:32:29 -0400

    Task 0. Tread lightly, she is near

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -27,11 +27,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	r = read(fd, buf, letters);
 	if (r == -1)
 	{
+		close(fd);
 		free(buf);
 		return (0);
 	}
 	w = write(STDOUT_FILENO, buf, r);
 
+	close(fd);
 	free(buf);
 	return (w);
 }
