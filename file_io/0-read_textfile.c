6c3509698f1b1fdde88eaddc26dfeb8039a53a85
andyavl <andyavl@hotmail.com>
2025-04-05 22:19:10 -0400

    Task 0. Tread lightly, she is near

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints ir to the POSIX
+ * @filename: file to read and print
+ * @letters: amount of letter to read and print
+ * Return: amount of letters it could read and print or 0 on error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	w = write(1, buf, r);
+
+	free(buf);
+	return (w);
+}
